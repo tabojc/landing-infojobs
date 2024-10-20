9e46ab28cae508e82ed43b6647f6251eff51d9b1
melissa <melissa.iman.n@uni.pe>
2024-10-20 12:19:47 -0500

    feat(bentoInfo): Added images and styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -13,11 +13,18 @@ export default {
         'ij-yellow': '#EFA500',
       },
       backgroundImage: {
-        'hero-pattern': 'url(/hero-pattern.webp)',
-        'kings-league': 'url(/kings-league-info.webp)',
-      },
+        'hero-pattern': 'url(/public/hero-pattern.webp)',
+        'kings-league': 'url(/public/kings-league-info.webp)',
+        'selection-process': "url('/public/bento-info/selection-process.webp'), linear-gradient(90deg, rgba(254,246,219,0.8) 0%, rgba(252,225,151,0.8) 100%)",
+        'emerging-positions': "url('/public/bento-info/emergent-positions.webp')",
+        'company-opinion': "url('/public/bento-info/company-opinion.webp')",
+        'no-experience': "url('/public/bento-info/no-experience.webp'), linear-gradient(139.81deg, rgb(230, 246, 238) 26.01%, rgb(139, 207, 173) 131.62%)"
+      },  
       borderRadius: {
         '4xl': '3rem',
+      },
+      backgroundSize: {
+        'auto-height': 'auto 100%', 
       },
       letterSpacing: {
         separated: '0.35px',
