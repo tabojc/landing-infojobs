c2927b1a561390e82eac98cb43e954c41c2219be
Melissa1221 <melissa.iman.n@uni.pe>
2024-10-21 10:34:08 -0500

    fix(KingsLeagueInfo): Hover image

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -14,7 +14,6 @@ export default {
       },
       backgroundImage: {
         "hero-pattern": "url(/hero-pattern.webp)",
-        "kings-league": "url(/kings-league-info.webp)",
       },
       borderRadius: {
         "4xl": "3rem",
